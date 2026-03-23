cc42364f17afe94c3e4794b6c047c10960ee5597
Jacob O'Neill <jacoboneill2000@outlook.com>
2026-03-23 16:41:19 +0000

    test: add missing auth test

--- internal/handlers/middleware_test.go
+++ internal/handlers/middleware_test.go
@@ -1,12 +1,23 @@
 package handlers
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/jacoboneill/SecureBin/internal/db"
+	"github.com/jacoboneill/SecureBin/internal/testutil"
 )
 
+func goodNext(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
+
 func TestHTMXMiddleware(t *testing.T) {
+	h := &Handler{}
+
 	tests := []struct {
 		name           string
 		isHTMX         bool
@@ -25,15 +36,82 @@ func TestHTMXMiddleware(t *testing.T) {
 
 			w := httptest.NewRecorder()
 
-			h := &Handler{}
-
-			h.htmx(func(w http.ResponseWriter, r *http.Request) {
-				w.WriteHeader(http.StatusOK)
-			})(w, r)
+			h.htmx(goodNext)(w, r)
 
 			if w.Code != tt.expectedStatus {
 				t.Errorf("expected status %d, got %d", tt.expectedStatus, w.Code)
 			}
 		})
 	}
+}
+
+func assertLoginRedirect(t testing.TB, w *httptest.ResponseRecorder) {
+	t.Helper()
+
+	resp := w.Result()
+	statusCode := resp.StatusCode
+	location := resp.Header.Get("Location")
+
+	if statusCode != http.StatusSeeOther {
+		t.Errorf("expected status %d, got %d", http.StatusSeeOther, statusCode)
+	}
+
+	if location != "/login" {
+		t.Errorf("expected redirect to /login, got %s", location)
+	}
+}
+
+func TestAuthMiddleware(t *testing.T) {
+	queries, _ := testutil.SetupTestDB(t)
+	user := testutil.SeedUser(t, queries)
+	h := New(queries)
+
+	t.Run("test no cookie", func(t *testing.T) {
+		w := httptest.NewRecorder()
+		r := httptest.NewRequest(http.MethodGet, "/", nil)
+
+		h.auth(goodNext)(w, r)
+
+		assertLoginRedirect(t, w)
+	})
+
+	t.Run("test invalid cookie", func(t *testing.T) {
+		w := httptest.NewRecorder()
+		r := httptest.NewRequest(http.MethodGet, "/", nil)
+		r.AddCookie(&http.Cookie{Name: "session", Value: "123456"})
+
+		h.auth(goodNext)(w, r)
+
+		assertLoginRedirect(t, w)
+	})
+
+	t.Run("test valid cookie", func(t *testing.T) {
+		token := make([]byte, 32)
+		rand.Read(token)
+
+		session, err := h.queries.CreateSession(t.Context(), db.CreateSessionParams{
+			ID:     base64.URLEncoding.EncodeToString(token),
+			UserID: user.ID,
+		})
+		if err != nil {
+			t.Fatalf("failed to create new session %q", err)
+		}
+
+		w := httptest.NewRecorder()
+		r := httptest.NewRequest(http.MethodGet, "/", nil)
+		r.AddCookie(&http.Cookie{Name: "session", Value: session.ID})
+
+		h.auth(goodNext)(w, r)
+		resp := w.Result()
+		statusCode := resp.StatusCode
+		location := resp.Header.Get("Location")
+
+		if statusCode != http.StatusOK {
+			t.Errorf("expected status %d, got %d", http.StatusOK, statusCode)
+		}
+
+		if location != "" {
+			t.Errorf("did not expect redirect, got redirected to %s", location)
+		}
+	})
 }
