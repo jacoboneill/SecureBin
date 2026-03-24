95488ba580041e3e5456f1901ae61ca5daeb0eb7
Jacob O'Neill <jacoboneill2000@outlook.com>
2026-03-24 13:57:27 +0000

    feat: add GetUser query

--- internal/db/queries/users.sql
+++ internal/db/queries/users.sql
@@ -1,5 +1,8 @@
+-- name: GetUser :one
+SELECT * FROM users WHERE id = ? LIMIT 1;
+
 -- name: GetUserByEmailOrUsername :one
-SELECT * FROM users WHERE email = sqlc.arg(identifier) OR username = sqlc.arg(identifier);
+SELECT * FROM users WHERE email = sqlc.arg(identifier) OR username = sqlc.arg(identifier) LIMIT 1;
 
 -- name: RegisterUser :one
 INSERT INTO users (username, email, password_hash, is_admin)
